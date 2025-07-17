768ad18c9eef1fc8e684db2863363387e8622366
Lord_Buks <lords.buks@gmail.com>
2025-07-17 15:11:34 -0300

    Update LoggedInWelcome.jsx

--- src/components/LoggedInWelcome.jsx
+++ src/components/LoggedInWelcome.jsx
@@ -74,8 +74,8 @@ const LoggedInWelcome = ({ user, onContinue, onAdminClick }) => {
               )}
             </div>
           </div>
-       
-         <p><Footer />
+          </div>
+       <Footer />
 
           <div className="text-sm text-gray-500">
           </div>
